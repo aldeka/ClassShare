54720ca508662d1a3b4204e30a3af7bdcbafac4d
Karen Rustad <karen.rustad@gmail.com>
2011-10-26 17:10:46 -0700

    added south app, made preliminary review models and got south to track and migrate them

--- classshare/settings.py
+++ classshare/settings.py
@@ -118,6 +118,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'reviews',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
