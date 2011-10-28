a9358ac5c777df9a9e3ae60d52a0b81bdb750c67
Rowyn McDonald <rowynm@gmail.com>
2011-10-28 13:05:57 -0700

    Updating data model and requirements.

--- classshare/reviews/models.py
+++ classshare/reviews/models.py
@@ -1,24 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
 
 # Create your models here.
 
-class ISchooler(User):
+class Student(User):
     '''Model for all of our users, inheriting Django's built-in User model'''
     # TODO: Make LDAP connect to this thing
     is_current_student = models.BooleanField(default=True)
 
+
 class Course(models.Model):
     '''Model for a given course'''
-    # TODO: decide if we want another model for each instance of the course ("Class?")
     name = models.CharField(max_length=200)
+    description = models.TextField(blank=True,null=True)
+    instructor = models.ManyToManyField(Instructor, through='Class')
+    
+
+class Class(models.Model):
+    '''A class is a specific instance of a course.'''
+    SEMESTER_CHOICES = (
+        ('Fall', 'Fall'),
+        ('Spring', 'Spring'),
+        ('Summer', 'Summer'),
+    )
+    course = models.ForeignKey(Course, blank=False)
+    instructor = models.Foreignkey(Instructor, blank=False)
+    year = models.IntegerField()
+    semester = models.CharField(max_length=6, choices=SEMESTER_CHOICES)
+
 
 class Review(models.Model):
     '''Model for a single person's review of a course'''
-    author = models.ForeignKey(ISchooler, blank=True, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(Student, blank=True, null=True, on_delete=models.SET_NULL)
     course = models.ForeignKey(Course)
     
     content = models.TextField(blank=True,null=True)
     is_anonymous = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Instructor(models.Model):
+    '''Model for a course instructor.'''
+    name = models.CharField(max_length=200)
