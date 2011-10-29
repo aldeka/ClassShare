11865bab4193e4aa330b199ba09e8b1f5f4a8ebb
Karen Rustad <karen.rustad@gmail.com>
2011-10-28 23:03:44 -0700

    added __unicode__ methods to models

--- classshare/reviews/models.py
+++ classshare/reviews/models.py
@@ -6,20 +6,32 @@ class Student(User):
     '''Model for all of our users, inheriting Django's built-in User model'''
     # TODO: Make LDAP connect to this thing
     is_alumnus = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return self.name
 
 class Instructor(models.Model):
     '''Model for a course instructor.'''
     name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return 'Instructor: ' + self.name
 
 class Department(models.Model):
     '''Model for an academic department.'''
     name = models.CharField(max_length=200, blank=True,null=True)
     # Official Abbreviation
     abb = models.CharField(max_length=10)
+    
+    def __unicode__(self):
+        return self.abb + ' department'
 
 class Tag(models.Model):
     '''Model for a tag'''
     name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return 'Tag: ' + self.name
 
 class Course(models.Model):
     '''Model for a given course'''
@@ -28,6 +40,9 @@ class Course(models.Model):
     department = models.ForeignKey(Department)
     number = models.CharField(max_length=12)
     tags = models.ManyToManyField(Tag, blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.department.abb + ' ' + self.number
 
 class Class(models.Model):
     '''A class is a specific instance of a course.'''
@@ -41,6 +56,13 @@ class Class(models.Model):
     year = models.IntegerField()
     ccn = models.IntegerField(blank=True,null=True)
     semester = models.CharField(max_length=6, choices=SEMESTER_CHOICES)
+    
+    def semester_formatted(self):
+        '''Returns a nicely formatted semester string, e.g. "Fall 2008"'''
+        return self.semester + ' ' + str(self.year)
+    
+    def __unicode__(self):
+        return self.course + ': ' + self.semester_formatted()
     
     def semester2date(self):
         '''Returns a datetime for the rough beginning date of the class' semester'''
@@ -61,3 +83,6 @@ class Review(models.Model):
     is_anonymous = models.BooleanField(default=False)
     thumbs_up = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return author.name + "'s review of " + self.reviewed_class
