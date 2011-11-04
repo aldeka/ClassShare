fc67901f6b01f8f7f53488214e4132dcc2aff1f6
Karen Rustad <karen.rustad@gmail.com>
2011-11-04 12:35:49 -0700

    in the middle of making add/edit functionality work

--- classshare/reviews/views.py
+++ classshare/reviews/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render_to_response
 from reviews.models import Course
+import re
 
 def home(request):
     courses = Course.objects.all()
     return render_to_response('index.html', {'courses' : courses })
+    
+def add_or_review_course(request):
+    # AUGH REGEX AUGHHHHH
+    if request.POST:
+        course_stub = request.POST('course-stub')
+        stub_dept = re.search('^([a-zA-Z])+[\ ]?[0-9a-zA-Z\-\ .]+', course_stub)
+        # check if this course already exists in database
+        if course_id:
+            review_course(request, course_id)
+        # if not, add a new course
+        else:
+            add_course(request, stub_dept, stub_num)
+        
+def add_course(request, dept=None, number=None):
+    return render_to_response('reviews/add_course_form.html', {'dept': dept, 'number': number})
+        
+def review_course(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    # the line below is redundant, but whatever
+    course_id = course.pk
+    return render_to_response('reviews/review_form.html', {'course_id' : course_id })
     
 def courses(request):
     courses = Course.objects.all()
--- classshare/urls.py
+++ classshare/urls.py
@@ -6,8 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'reviews.views.home', name='home'),
     url(r'^courses/$', 'reviews.views.courses', name='courses'),
+    url(r'^courses/add/$', 'reviews.views.add_or_review_course', name='add_or_review_course'),
     url(r'^courses/(?P<course_id>\d+)/$', 'reviews.views.course', name="course"),
-    
+    url(r'^courses/(?P<course_id>\d+)/review/$', 'reviews.views.write_new_review', name="write_new_review"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
