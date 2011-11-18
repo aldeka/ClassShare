bd47f13f14b18f496094aeee57c8b78fea176e65
Karen Rustad <karen.rustad@gmail.com>
2011-11-18 14:34:35 -0800

    formatted urls to make them easier to read

--- classshare/urls.py
+++ classshare/urls.py
@@ -6,18 +6,26 @@ admin.autodiscover()
 urlpatterns = patterns('reviews.views',
     url(r'^$', 'home', name='home'),
     url(r'^courses/$', 'courses', name='courses'),
+    
     url(r'^courses/add_or_review/$', 'add_or_review_course', name='add_or_review_course'),
     url(r'^courses/add/$', 'add_course', name='add_course'),
+    
     url(r'^courses/(?P<course_id>\d+)/$', 'course', name="course"),
     url(r'^courses/(?P<course_id>\d+)/choose_class/$', 'choose_class_to_review', name='choose_class'),
     url(r'^courses/(?P<class_id>\d+)/review/$', 'review_course', name="review_course"),
+    url(r'^courses/(?P<class_id>\d+)/review/(?P<review_id>\d+)/edit/$', 'edit_review', name="edit_review"),
+    
     url(r'^depts/$', 'departments', name='departments'),  
     url(r'^depts/(?P<dept_abb>.+)/$', 'department', name='department'),
+    
     url(r'^instructors/$', 'instructors', name='instructors'),     
     url(r'^instructors/(?P<instructor_id>\d+)/$', 'instructor', name='instructor'),
+    
     url(r'^tags/$', 'tags', name='tags'),
     url(r'^tags/(?P<tag_id>\d+)/$', 'tag', name='tag'),
+    
     url(r'^allreviews/$', 'reviews', name='reviews'),
+    
     url(r'^students/$', 'students', name='students'),     
     url(r'^students/(?P<student_id>\d+)/$', 'student', name='student'),
 
